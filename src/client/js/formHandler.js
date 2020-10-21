5aa392d8006606f6a93e854c04ede866c63e9a8b
barbaracrp <ba84.enf09@gmail.com>
2020-10-21 19:57:48 -0400

    add basic html

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,28 @@
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    const formText = document.getElementById('name').value
+    if (formText.trim().length === 0) {
+        alert("type a phrase or a word")
+        return;
+    }
+    Client.checkForName(formText)
+    const newObject = {text: formText}
+
+    console.log("::: Form Submitted :::")
+    fetch('http://localhost:8080/sentiment', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newObject),
+      })
+    .then(res => res.json())
+    .then(function(res) {
+        document.getElementById('results').innerHTML = res.irony
+    })
+}
+
+export { handleSubmit };
---
+++ src/server/server.js
@@ -0,0 +1,71 @@
+// //Geonames
+// const apiKey =
+// const baseURL =
+
+// //Weatherbit
+// const apiKey =
+// const baseURL =
+
+// //Pixabay
+// const apiKey =
+// const baseURL =
+
+
+
+
+// Setup empty JS object to act as endpoint for all routes
+const projectData = {
+  tempEntry: []
+};
+
+// Require Express to run server and routes
+const express = require('express'); //importando o express
+
+// Start up an instance of app
+const app = express(); // servidor express
+
+const bodyParser = require('body-parser')
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false })); // para entender urls que trazem parametros
+app.use(bodyParser.json()); // para entender requisicóes json
+
+// Cors for cross origin allowance
+const cors = require('cors'); // package q permite browser e server conversar um com o outro
+app.use(cors());              // sem secutity interrupcoes
+
+// Initialize the main project folder
+// allows us to write server-side code that can them connect to client-side code wich would be in a folder called website
+app.use(express.static('website')); //pointed our app to the folder that we want them to look at.(connects our server-side code (server.js) to our client side code (website)folder)
+
+// Setup Server
+const port = 3000;
+const server = app.listen(port, listening); //fala pro server qual porta e callback func
+
+function listening(){
+  console.log('server running');
+  console.log(`running on localhost: ${port}`);
+};
+
+//GET route
+// There should be a GET route setup on the server side with the first argument as a string naming the route,
+//and the second argument a callback function to return the JS object created at the top of server code.
+app.get('/all', sendData);
+function sendData(req, res) {
+  res.send(projectData);
+};
+
+//POST route
+//You should be able to add an entry to the project endpoint using a POST route setup on the server side
+//and executed on the client side as an asynchronous function.
+//The server side function should create a new entry in the apps endpoint (the named JS object) consisting
+//of the data received from the client side POST.
+app.post('/add', function addData(req, res) {
+  const newEntry = {
+    "date" : req.body.date,
+    "temp" : req.body.temp,
+    "feelings" : req.body.feelings,
+  };
+  projectData.tempEntry.push(newEntry);
+  res.send(projectData.tempEntry);
+});
